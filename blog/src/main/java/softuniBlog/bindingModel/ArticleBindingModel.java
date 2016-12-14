377e6a79b5b93374039e0b6f8cf3cdaf70f125c2
katrinpopova <katrin.mihaylova@gmail.com>
2016-12-14 02:05:32 +0200

    project upload

---
+++ blog/src/main/java/softuniBlog/bindingModel/ArticleBindingModel.java
@@ -0,0 +1,50 @@
+package softuniBlog.bindingModel;
+
+
+import javax.validation.constraints.NotNull;
+
+public class ArticleBindingModel {
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String content;
+
+    private Integer CategoryId;
+
+    private String TagString;
+
+
+    public Integer getCategoryId() {
+        return CategoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        CategoryId = categoryId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTagString() {
+        return TagString;
+    }
+
+    public void setTagString(String tagString) {
+        TagString = tagString;
+    }
+}
